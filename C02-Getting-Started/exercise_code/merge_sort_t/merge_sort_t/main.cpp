44b9e3aa8b7bed244c8df912a0f66d4c6208998f
yongjies <yongjies@cadence.com>
2021-02-05 15:16:07 +0800

    hello

---
+++ C02-Getting-Started/exercise_code/merge_sort_t/merge_sort_t/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  merge_sort_t
+//
+//  Created by 孙永杰 on 2021/2/5.
+//
+
+#include <iostream>
+/*
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+*/
